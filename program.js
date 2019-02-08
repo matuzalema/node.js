69fb3928200bb4667c30c96b8cdf84754687df6b
Jadwiga Bochinska <matuzalema@gmail.com>
2019-02-08 17:15:30 +0100

    Delete console.log and comments

--- program.js
+++ program.js
@@ -1,29 +1,21 @@
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
-        var nodeInf = process.versions.node;
-        process.stdout.write('Node version: ' + nodeInf);
+    var nodeInf = process.versions.node;
+    process.stdout.write('Node version: ' + nodeInf);
 
-        var systemLanguage = process.env.LANG;
-        // console.log(systemLanguage);
+    var systemLanguage = process.env.LANG;
 
-        var system = process.platform;
-        // console.log(system);
+    var system = process.platform;
 
-        switch (system)
-        {
-            // case "win32":
-            //     console.log(systemLanguage);
-            //     process.stdout.write('Twoj jezyk to:' + systemLanguage);
-            //     break;
-            case "macOS":
-                // console.log(systemLanguage);
-                process.stdout.write('Twoj system to:' + systemLanguage);
-                break;
-            case "Linux":
-                // console.log(systemLanguage);
-                process.stdout.write('Twoj system to:' + systemLanguage);
-                break;
-        }
+    switch(system)
+    {
+
+    case "macOS":
+        process.stdout.write('Twoj system to:' + systemLanguage);
+        break;
+    case "Linux":
+        process.stdout.write('Twoj system to:' + systemLanguage);
+        break;
+    }
 
     var input = process.stdin.read();
     if (input !== null) {
